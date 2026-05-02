a5b56b794334ff480e480826ab3249d94c5995dc
Alfonso Perea <apereamorillo@gmail.com>
2026-05-02 17:02:16 +0200

    VideoMiner Repos

---
+++ VideoMiner/src/main/java/aiss/videominer/repository/ChannelRepository.java
@@ -0,0 +1,7 @@
+import aiss.videominer.model.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChannelRepository extends JpaRepository<Channel, String> {
+}
---
+++ VideoMiner/src/main/java/aiss/videominer/repository/VideoRepository.java
@@ -0,0 +1,7 @@
+import aiss.videominer.model.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, String> {
+}
